8056ef1869865dc221c701a0734c1d713b8a287d
montenegrosol260 <montenegrosol260@gmail.com>
2025-11-11 22:56:49 -0300

    Initial commit

--- src/main/java/com/utn/productos_api/dto/ActualizarStockDTO.java
+++ src/main/java/com/utn/productos_api/dto/ActualizarStockDTO.java
@@ -2,9 +2,11 @@ package com.utn.productos_api.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "DTO para actualizar únicamente el stock de un producto")
 public record ActualizarStockDTO(
-
+        @Schema(description = "La nueva cantidad de unidades en inventario", example = "75")
         @NotNull(message = "El stock no puede ser nulo")
         @Min(value = 0, message = "El stock no puede ser negativo")
         Integer stock
--- src/main/java/com/utn/productos_api/dto/ProductoResponseDTO.java
+++ src/main/java/com/utn/productos_api/dto/ProductoResponseDTO.java
@@ -2,13 +2,21 @@ package com.utn.productos_api.dto;
 
 import com.utn.productos_api.model.Categoria;
 import com.utn.productos_api.model.Producto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "DTO para enviar los detalles completos de un producto")
 public record ProductoResponseDTO(
+        @Schema(description = "ID único del producto en la base de datos", example = "1")
         Long id,
+        @Schema(description = "Nombre del producto", example = "Monitor Gamer Curvo 27\"")
         String nombre,
+        @Schema(description = "Descripción detallada del producto", example = "Monitor 144Hz 1ms...")
         String descripcion,
+        @Schema(description = "Precio de venta del producto", example = "299.99")
         Double precio,
+        @Schema(description = "Cantidad de unidades en inventario", example = "50")
         Integer stock,
+        @Schema(description = "Categoría a la que pertenece el producto", example = "ELECTRONICA")
         Categoria categoria
 ) {
 
